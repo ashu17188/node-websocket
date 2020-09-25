bc5d054e857acdaa87db3ea8ca23ad2ed920b7e5
Ashutosh <ashutosh.shukla@innovect.com>
2020-09-25 17:36:51 +0530

    1.ws npm usage as socket library.

--- client.ts
+++ client.ts
@@ -1,34 +1,12 @@
-#!/usr/bin/env node
-import * as websocket from 'websocket';
+const WebSocket = require('ws');
+import {Server} from 'ws';
 
-let client = new websocket.client();
+const ws = new WebSocket('ws://localhost:8080/demo');
  
-client.on('connectFailed', (error) => {
-    console.log('Connect Error: ' + error.toString());
+ws.on('open', function open() {
+  ws.send('something');
 });
  
-client.on('connect', (connection)=> {
-    console.log('WebSocket Client Connected');
-    connection.on('error', (error) =>{
-        console.log("Connection Error: " + error.toString());
-    });
-    connection.on('close', () => {
-        console.log('echo-protocol Connection Closed');
-    });
-    connection.on('message', (message) => {
-        if (message.type === 'utf8') {
-            console.log("Received: '" + message.utf8Data + "'");
-        }
-    });
-    
-    function sendNumber() {
-        if (connection.connected) {
-            var number = Math.round(Math.random() * 0xFFFFFF);
-            connection.sendUTF(number.toString());
-            setTimeout(sendNumber, 1000);
-        }
-    }
-    sendNumber();
+ws.on('message', function incoming(data: any) {
+  console.log(data);
 });
- 
-client.connect('ws://localhost:8080/', 'echo-protocol');
--- server.ts
+++ server.ts
@@ -1,56 +1,31 @@
 #!/usr/bin/env node
-import * as websocket from 'websocket';
+import * as WebSocket from 'ws';
 import { createServer } from 'http';
 
-let WebSocketServer = websocket.server;
-
-let server = createServer((req, res) => {
+//const fs = require('fs');
+//const https = require('https');
+ 
+let server  = createServer((req, res) => {
     console.log((new Date()) + ' Received request for ' + req.url);
     res.writeHead(404);
     res.end();
 });
 
+/// const server = https.createServer({
+//   cert: fs.readFileSync('/path/to/cert.pem'),
+//   key: fs.readFileSync('/path/to/key.pem')
+// });
+
+let wss:WebSocket.Server = new WebSocket.Server({ server });
+ 
+wss.on('connection', (request:any) => {
+  request.on('message', function incoming(message:any) {
+    console.log('received: %s', message);
+  });
+ 
+  request.send('something');
+});
+ 
 server.listen(8080, () => {
     console.log((new Date()) + ' Server is listening on port 8080');
-});
-
-let wsServer: websocket.server = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false,
-});
-
-function originIsAllowed(origin: string) {
-    // put logic here to detect whether the specified origin is allowed.
-    return true;
-}
-
-
-wsServer.on('request', (request) => {
-    if (!originIsAllowed(request.origin)) {
-        // Make sure we only accept requests from an allowed origin
-        request.reject();
-        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-        return;
-    }
-
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', (message: websocket.IMessage) => {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data as websocket.IStringified);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData?.byteLength + ' bytes');
-            connection.sendBytes(message.binaryData as Buffer);
-        }
-    });
-    connection.on('close', (reasonCode, description) => {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
 });
